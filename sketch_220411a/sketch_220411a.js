565723138121bf35e9edd2fb4567e10f3e2838cf
Gusgotoyu365 <gusgotoyu365@naver.com>
2022-04-14 14:35:30 +0900

    바다의 바닥 구현

--- sketch_220411a/sketch_220411a.js
+++ sketch_220411a/sketch_220411a.js
@@ -7,14 +7,15 @@ let centerY;
 let centerZ;
 let hi = 20;
 
-var cols, rows;
-var scl = 20;
-var w = 1400;
-var h = 1000;
+let cols, rows;
+let scl = 20;
+let w = 1400;
+let h = 1000;
+const SB = -70; //Sea bottom
 
-var flying = 0;
+let sb_speed = 0;
 
-var terrain = [];
+let terrain = [];
 
 function setup() {
   createCanvas(540, 960, WEBGL);
@@ -23,10 +24,9 @@ function setup() {
   cols = 20;
   rows = 20;
   
-  
-  for (var x = 0; x < cols; x++) {
+  for (let x = 0; x < cols; x++) {
     terrain[x] = [];
-    for (var y = 0; y < rows; y++) {
+    for (let y = 0; y < rows; y++) {
       terrain[x][y] = 0; //specify a default value for now
     }
   }
@@ -46,47 +46,85 @@ function setup() {
 function draw() {
   rotateX(PI/2);
   //땅  
-  var yoff = flying;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -50, 50);
-      xoff += 0.2;
-    }
-    yoff += 0.2;
-  }
-
-  background(0);
-  //translate(0, 50);
-  normalMaterial();
-  fill(242,245,169);
-  //translate(-w / 2, -h / 2);
-  for (var y = 0; y < rows - 1; y++) {
-    beginShape(TRIANGLE_STRIP);
-    for (var x = 0; x < cols; x++) {
-      vertex(x * scl, y * scl, terrain[x][y]);
-      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
-    }
-    endShape();
-    
-  }
+  let yoff = sb_speed;
   
+  sb_display(yoff);
+  
+  slider();
+  
+  //camera(X, Y, Z, centerX, centerY, centerZ, 0, 1, 0);
+  //print("X: "+X+"  Y: "+Y+"  Z: "+Z+"\ncX: "+centerX+"  cY: "+centerY+"  cZ: "+centerZ);
+  camera(1000, -187, 656, -94, -125, 0);
+  //camera(-625, 813, 1000, 281, -406, 125);
+}
+
+function slider() {
   X = sliderGroup[0].value();
   Y = sliderGroup[1].value();
   Z = sliderGroup[2].value();
   centerX = sliderGroup[3].value();
   centerY = sliderGroup[4].value();
   centerZ = sliderGroup[5].value();
-  //camera(X, Y, Z, centerX, centerY, centerZ, 0, 1, 0);
-  //print("X: "+X+"  Y: "+Y+"  Z: "+Z+"\ncX: "+centerX+"  cY: "+centerY+"  cZ: "+centerZ);
-  camera(1000, -187, 656, -94, -125, 0);
 }
 
-//카메라 시점
-/*X: 1125  Y: -375  Z: 625
-cX: -125  cY: -375  cZ: 0 */
-    
-/*X: 1100  Y: -250  Z: 625
-cX: -62  cY: -156  cZ: 0 */
-    
-/*1000 -187 656 -94 -125 0*/
+function sb_display(yoff) {
+  for (let y = 0; y < rows; y++) {
+    let xoff = 0;
+    for (let x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -50, 50);
+      xoff += 0.2;
+    }
+    yoff += 0.2;
+  }
+  
+  push();
+  background(0);
+  normalMaterial();
+  fill(242,245,169);
+  for (let y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < cols; x++) {
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+    }
+    endShape();
+  }
+  
+  for (let x = 0; x < cols; x++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let y = 0; y < rows - 1; y++) {
+      if (x == 0 || x == cols - 1) {
+        vertex(x * scl, y * scl, terrain[x][y]);
+        vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+        vertex(x * scl, y * scl, SB);
+        vertex(x * scl, (y + 1) * scl, SB);
+      }
+    }
+    endShape();
+  }
+  
+  for (let y = 0; y < rows; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < rows - 1; x++) {
+      if (y == 0 || y == rows - 1) {
+        vertex(x * scl, y * scl, terrain[x][y]);
+        vertex((x + 1) * scl, y * scl, terrain[x + 1][y]);
+        vertex(x * scl, y * scl, SB);
+        vertex((x + 1) * scl, y * scl, SB);
+      }
+    }
+    endShape();
+  }
+  
+  beginShape();
+  vertex(0, 0, SB);
+  vertex(0, 19 * scl, SB);
+  vertex(19 * scl, 19 * scl, SB);
+  vertex(19 * scl, 0, SB);
+  endShape(CLOSE);
+  pop();
+}
+
+function sea_display() {
+  
+}
